671f787fc230183e6036f79d3ac4a1ac415399d0
Prajwal Uppal <prajwaluppal@Prajwals-MacBook-Pro.local>
2026-08-26 08:58:39 +0930

    Implemented Karatsuba

--- Addition.cpp
+++ Addition.cpp
@@ -6,62 +6,33 @@
 
 using namespace std;
 
-int addition(int a, int b, int base)
-{
-  int result = 0, carry = 0, pos = 1;
-
-  while (a > 0 || b > 0)
-  {
-    int digita, digitb, digitres;
-    digita = a % 10;
-    digitb = b % 10;
-    digitres = digita + digitb + carry;
-    carry = 0;
-    if (digitres >= base)
-    {
-      carry = 1;
-      digitres = digitres % base;
-    }
-
-    int posCopy = pos;
-    while (posCopy > 1)
-    {
-      digitres *= 10;
-      posCopy--;
-    }
-    result += digitres;
-    a /= 10;
-    b /= 10;
-    pos++;
-  }
-
-  if (carry == 1)
-  {
-    while (pos > 1)
-    {
-      carry *= 10;
-      pos--;
-    }
-    result += carry;
-  }
-
-  return result;
-}
-
-vector<int> additionVec(vector<int> a, vector<int> b, int base)
+vector<int> addition(vector<int> a, vector<int> b, int base)
 {
   // Define carry var and result vector
   int carry = 0;
   vector<int> result;
 
-  // Iterates while both vectors have integers
-  while (~a.empty() && ~b.empty())
+  // Iterates while both vectors have integers, or carry exists
+  while (!a.empty() || !b.empty() || carry)
   {
     int digitA, digitB, digitRes;
 
-    // Take LSD of both vectors
-    digitA = a.back();
-    digitB = b.back();
+    // Assume both digits 0 at first
+    digitA = 0;
+    digitB = 0;
+
+    // Assign digit A if not empty
+    if (!a.empty())
+    {
+      digitA = a.back();
+      a.pop_back();
+    }
+    // Assign digit B if not empty
+    if (!b.empty())
+    {
+      digitB = b.back();
+      b.pop_back();
+    }
     // Sum
     digitRes = digitA + digitB + carry;
     carry = 0;
@@ -73,45 +44,52 @@ vector<int> additionVec(vector<int> a, vector<int> b, int base)
     }
     // Add onto vector
     result.push_back(digitRes);
+  }
+
+  // Reverse the vector to present in MSD to LSD form.
+  reverse(result.begin(), result.end());
+  return result;
+}
+
+vector<int> subtraction(vector<int> a, vector<int> b, int base)
+{
+  // Define borrow var and result vector
+  int borrow = 0;
+  vector<int> result;
+
+  // Iterates while a not empty
+  while (!a.empty())
+  {
+    int digitA, digitB, digitRes;
+
+    // Take LSD of both vectors
+    digitA = a.back();
+    digitB = 0;
+    if (!b.empty())
+    {
+      digitB = b.back();
+      b.pop_back();
+    }
+
+    // Sum
+    digitRes = digitA - digitB - borrow;
+    borrow = 0;
+    // Account for borrow
+    if (digitRes < 0)
+    {
+      digitRes += base;
+      borrow = 1;
+    }
+    // Add onto vector
+    result.push_back(digitRes);
     // Pop the back to get the next LSD
     a.pop_back();
-    b.pop_back();
   }
 
-  // If a was not empty, b must be empty.
-  // fill result with remaining a digits, making note of carry
-  while (~a.empty())
+  // Remove leading zeros
+  while (result.size() > 1 && result.back() == 0)
   {
-    int digitA = a.back();
-    int digitRes = digitA + carry;
-    if (digitRes >= base)
-    {
-      digitRes %= base;
-      carry = 1;
-    }
-    result.push_back(digitRes);
-    a.pop_back();
-  }
-  // If a was empty, fill result with remaining B digits, making note of
-  // carry
-  while (~b.empty())
-  {
-    int digitB = b.back();
-    int digitRes = digitB + carry;
-    if (digitRes >= base)
-    {
-      digitRes %= base;
-      carry = 1;
-    }
-
-    result.push_back(digitRes);
-    b.pop_back();
-  }
-
-  // Account for carry at the end
-  if (carry == 1)
-  {
-    result.push_back(1);
+    result.pop_back();
   }
 
   // Reverse the vector to present in MSD to LSD form.
@@ -123,46 +101,61 @@ vector<int> Karatsuba(vector<int> a, vector<int> b, int base)
 {
   vector<int> result;
   vector<int> prod1, prod2, prod3;
+  int maxLen = max(a.size(), b.size());
+
   int mid = max(a.size(), b.size()) / 2;
 
-  if (a.size() <= 1 && b.size() <= 1)
+  // Base Case if both 1 digit long
+  if (a.size() == 1 && b.size() == 1)
   {
     int res = a.front() * b.front();
-    result.push_back(res / base);
-    result.push_back(res % base);
+    if (res == 0)
+    {
+      return {0};
+    }
+
+    while (res > 0)
+    {
+      result.push_back(res % base);
+      res /= base;
+    }
+
+    reverse(result.begin(), result.end());
     return result;
   }
 
-  if (a.size() <= mid)
+  // Make both vectors the same size by adding leading zeros
+  if (a.size() < maxLen)
   {
-    vector<int> b0(b.begin(), b.begin() + mid);
-    vector<int> b1(b.begin() + mid + 1, b.end());
-    prod1 = Karatsuba(b0, a, base);
-    prod2 = Karatsuba(b1, a, base);
-    prod1.insert(prod1.end(), mid, 0);
-    result = additionVec(prod1, prod2, base);
-    return result;
+    a.insert(a.begin(), maxLen - a.size(), 0);
   }
-  else if (b.size() <= mid)
+
+  if (b.size() < maxLen)
   {
-    vector<int> a0(a.begin(), a.begin() + mid);
-    vector<int> a1(a.begin() + mid + 1, a.end());
-    prod1 = Karatsuba(a0, b, base);
-    prod2 = Karatsuba(a1, b, base);
-    prod1.insert(prod1.end(), mid, 0);
-    result = additionVec(prod1, prod2, base);
-    return result;
+    b.insert(b.begin(), maxLen - b.size(), 0);
   }
-  else
-  {
-    vector<int> a0(a.begin(), a.begin() + mid);
-    vector<int> a1(a.begin() + mid + 1, a.end());
-    vector<int> b0(b.begin(), b.begin() + mid);
-    vector<int> b1(b.begin() + mid + 1, b.end());
-    prod1 = Karatsuba(a0, b0, base);
-    prod2 = Karatsuba(a1, b1, base);
-    prod3 = Karatsuba(additionVec(a1, a0, base), additionVec(b1, b0, base), base);
-  }
+
+  vector<int> a0(a.begin(), a.begin() + mid);
+  vector<int> a1(a.begin() + mid + 1, a.end());
+  vector<int> b0(b.begin(), b.begin() + mid);
+  vector<int> b1(b.begin() + mid + 1, b.end());
+  prod1 = Karatsuba(a0, b0, base);
+  prod3 = Karatsuba(a1, b1, base);
+
+  vector<int> sumA = addition(a0, a1, base);
+  vector<int> sumB = addition(b0, b1, base);
+
+  prod2 = Karatsuba(sumA, sumB, base);
+
+  prod2 = subtraction(prod3, prod1, base);
+  prod2 = subtraction(prod3, prod2, base);
+
+  prod1.insert(prod1.begin(), 2 * mid, 0);
+  prod2.insert(prod2.begin(), mid, 0);
+
+  result = addition(prod1, prod2, base);
+  result = addition(result, prod3, base);
+  return result;
 }
 
 int main(void) {}
